ddce73bc2cf35e2453d16864ac675eabff1e7c6c
Lars Saalbrink <larssaalbrink@gmail.com>
2024-03-11 11:35:57 +0100

    Display devices from default config

--- config_manager_client/src/App.tsx
+++ config_manager_client/src/App.tsx
@@ -1,11 +1,24 @@
-//import { useState } from "react";
-import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Device, Device_data } from "./Device";
+import styles from "./App.module.css";
+import default_config_json from "./assets/default_config.json";
 
 function App() {
+    const [devices, setDevices] = useState<Device_data[]>([]);
+
+    useEffect(() => {
+        // Hook to run once on component mount
+        setDevices(default_config_json.devices);
+    }, []);
+
+    // Render devices
     return (
-        <>
-            <div></div>
-        </>
+        <div className={styles.appContainer}>
+            {[...Array(devices.length)].map((_, i) => (
+                <Device key={i} {...devices[i]} />
+            ))}
+        </div>
     );
 }
 
